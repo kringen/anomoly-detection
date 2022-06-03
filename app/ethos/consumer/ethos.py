29e0c22cfc5fc31a3ed18401f087e70bc613c5a2
Erik Kringen <ekringen@gmail.com>
2022-06-02 22:36:58 -0400

    Updating server connections

--- app/ethos/consumer/ethos.py
+++ app/ethos/consumer/ethos.py
@@ -10,14 +10,17 @@ import datetime
 
 logging.basicConfig(stream=sys.stdout, format='%(asctime)s %(levelname)-7s ' +
             '%(threadName)-15s %(message)s', level=logging.INFO)
-r = redis.Redis(host='localhost', port=6379)
-rabbitmq_connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
 
 # Get configs from file-
 cwd = os.path.dirname(__file__)
 config_file = os.path.join(cwd,"config.yaml")
 with open(config_file, "r") as configfile:
             config = yaml.safe_load(configfile)
+
+redis_server = config["redis"]["server"]
+r = redis.Redis(host=redis_server, port=6379)
+rabbitmq_server = config["rabbitmq"]["server"]
+rabbitmq_connection = pika.BlockingConnection(pika.ConnectionParameters(host=redis_server))
 
 def generateCallback(q):
     def callback(ch, method, properties, body):
